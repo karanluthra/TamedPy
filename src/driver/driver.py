24e6e3809de41e9f0cf7888b627fc9a56a310e02
Karan Luthra <karanluthra06@gmail.com>
2018-11-12 15:59:27 -0500

    Allow reading files back from an execution directory via Result API

--- src/driver/driver.py
+++ src/driver/driver.py
@@ -168,7 +168,8 @@ class Worker(object):
     def execute(self, code):
         response = exec_http_req(self.port, self.id, code)
         assert response.ok
-        return response.text
+        result = Result(self.execd_path, self.id, response.text)
+        return result
 
     def on_container_finished(self):
         print("worker {} cleaning up".format(self.id))
@@ -177,6 +178,27 @@ class Worker(object):
         print("worker {} exiting".format(self.id))
         self.parent_driver.on_worker_exiting(self)
         print("worker {} exited".format(self.id))
+
+
+class Result(object):
+    def __init__(self, path, exec_id, stdout):
+        self.path = path
+        self.exec_id = exec_id
+        self._stdout = stdout
+
+    def __repr__(self):
+        return "[" + str(self.exec_id) + "] " + self.stdout()
+
+    def stdout(self):
+        return str(self._stdout)
+
+    def readFile(self, filename):
+        # FIXME: return a generator instead
+        filecontent = None
+        filepath = os.path.join(self.path, filename)
+        with open(filepath, "r") as f:
+            filecontent = f.read()
+        return filecontent
 
 
 def exec_http_req(port, execid, code):
@@ -202,6 +224,10 @@ with open("input.txt", "r") as f:
     '''
     ifp = '/tmp/input.txt'
     result = driver.execute(code, [ifp, ])
+    print(result)
+    print type(result)
     print("Output: " + str(result))
+
+    print(result.readFile("uppercase.txt"))
 
     driver.turndown()
